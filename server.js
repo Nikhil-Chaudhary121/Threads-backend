38d7b125713693181d11e2b13610d93af670e033
Nikhil-Chaudhary121 <Nikhilchaudhary9887@gmail.com>
2024-06-14 02:17:55 +0530

    final commit

--- server.js
+++ server.js
@@ -24,8 +24,31 @@ app.use(express.urlencoded({ extends: true }));
 app.use(cookieParser());
 
 app.use(cors());
-app.options("/api/users/", cors());
-app.options("/api/posts/", cors());
+
+app.options("/api/users/profile:query", cors());
+app.options("/api/users/signup", cors());
+app.options("/api/users/login", cors());
+app.options("/api/users/logout", cors());
+app.options("/api/users/follow/:id", cors());
+app.options("/api/users/update/:id", cors());
+app.options("/api/users/search", cors());
+
+router.get("/feed", protectRoute, getFeedPosts);
+router.post("/create", protectRoute, createPost);
+// router.get("/", postList);
+router.get("/:id", getPost);
+router.get("/user/:username", getUserPost);
+router.delete("/:id", protectRoute, deletePost);
+router.put("/like/:id", protectRoute, likeUnlikePost);
+router.put("/reply/:postId", protectRoute, replyToPost);
+
+app.options("/api/posts/feed", cors());
+app.options("/api/posts/create", cors());
+app.options("/api/posts/:id", cors());
+app.options("/api/posts/user/:username", cors());
+app.options("/api/posts/like/:id", cors());
+app.options("/api/posts/reply/:postId:", cors());
+
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
